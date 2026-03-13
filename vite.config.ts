659c96f6dcf7f4161306d8ccc0edcab8d591050f
Ampasocial <estrategiavitalcoaching@gmail.com>
2026-03-13 11:51:43 +0000

    v3

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    // Especifica la base para despliegue en GitHub Pages
+    base: '/Diagnosticoempleabilidad/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
